b37f4016d2515b77f9d1c808b03ce1fb7664bbb6
eline nea <daffamfd27@gmail.com>
2022-03-12 10:11:24 +0700

    *tugas*

---
+++ TUGAS 3 WGTIK-MUHAMMAD FARRAS DAFFA-1301204186.py
@@ -0,0 +1,9 @@
+Python 3.9.2 (tags/v3.9.2:1a79785, Feb 19 2021, 13:44:55) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print"Hello World"
+SyntaxError: invalid syntax
+>>> print("Hello World")
+Hello World
+>>> print("Tugas 3 WGTIK : Muhammad Farras Daffa (1301204186)")
+Tugas 3 WGTIK : Muhammad Farras Daffa (1301204186)
+>>> 
